c1886ba723166d27324fe65a8a5f5d6bd454bbb4
MuriloDevtestes <141646951+MuriloDevtestes@users.noreply.github.com>
2023-08-07 12:09:12 -0300

    tes

--- src/components/Preloader/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Preloader() {
-  return (
-    <div className="progress blue lighten-4">
-      <div className="indeterminate blue" />
-    </div>
-  );
-}
